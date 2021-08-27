268980518566606ab73cd3a3a2d9d33af6137e67
Levi Kohler <kohler.la01@gmail.com>
2021-08-27 15:18:41 -0700

    add style to homepage and create state for word and meanings

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import axios from 'axios';
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { Container } from '@material-ui/core';
 
 function App() {
+
+  const [word, setWord] = useState("");
+  const [meanings, setMeanings] = useState([]);
 
   const dictionaryApi = async () => {
     try {
       const data = await axios.get("https://api.dictionaryapi.dev/api/v2/entries/en/plane");
 
       console.log(data);
+      setMeanings(data.data)
 
     } catch (error) {
       console.log(error)
     }
   };
+
+  console.log(meanings);
 
   useEffect(() => {
     dictionaryApi();
@@ -21,7 +28,16 @@ function App() {
 
 
   return (
-    <div className="App"></div>
+    <div className="App" style={{ height: "100vh", backgroundColor: "#282c34", color: 'white' }}>
+
+      <Container maxWidth="md" style={{
+        display: "flex", flexDirection: "column", height: "100vh"
+      }}>
+
+        Dictionary
+
+      </Container>
+    </div>
   );
 }
 

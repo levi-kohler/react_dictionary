e282effb5487bd3331ee725e2440afb67ffe7505
Levi Kohler <kohler.la01@gmail.com>
2021-08-27 15:02:34 -0700

    call dictionary api in a console log

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import axios from 'axios';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
 
+  const dictionaryApi = async () => {
+    try {
+      const data = await axios.get("https://api.dictionaryapi.dev/api/v2/entries/en/plane");
+
+      console.log(data);
+
+    } catch (error) {
+      console.log(error)
+    }
+  };
+
+  useEffect(() => {
+    dictionaryApi();
+  }, [])
 
 
   return (
